97ad6b012a81818da247750b354d8c1cfd104203
chbk1992 <chbk1992@gmail.com>
2018-09-27 18:38:25 +0000

    initial java commit

---
+++ src/Rectangulator.java
@@ -0,0 +1,12 @@
+public class Rectangulator{
+	public static void main(string[]args){
+		int lenght = integer.parseInt(args[0]);
+		int width = integer.parseInt(args[1]);
+		
+		Rectangle myRectangle = new Rectangle(length, width);
+		
+		string output = string.format("*** Your Rectangle ***\n\nLenght: %d\nWidth: %d\nArea: %d\nPerimeter: %d/n/n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter())
+
+	system.out.println(output);
+	}
+}
